4245ccfebfad796e53d72f17be93d2a077343e9e
Adit Khandelwal <68413603+AditKhandelwal@users.noreply.github.com>
2025-02-20 00:16:00 -0800

    Created Project Files and Directories

---
+++ config/config.py
@@ -0,0 +1,7 @@
+GRAPHQL_ENDPOINT = "https://www.ratemyprofessors.com/graphql"
+UC_MERCED_SCHOOL_ID = "4767" 
+
+HEADERS = {
+    "Content-Type": "application/json",
+    "User-Agent": "Mozilla/5.0",
+}
